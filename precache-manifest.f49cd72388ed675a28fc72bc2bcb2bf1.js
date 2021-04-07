9cc0cf914268f871a597cdcea0fd278fdecff114
D-Choksi <choksi.david@gmail.com>
2021-04-07 03:32:18 -0400

    Updates

---
+++ precache-manifest.f49cd72388ed675a28fc72bc2bcb2bf1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "507a25a68045fef01b4acb21e7f174ef",
+    "url": "/NetflixClone/index.html"
+  },
+  {
+    "revision": "f65620f21bb2e48d5351",
+    "url": "/NetflixClone/static/css/main.ce5f8fca.chunk.css"
+  },
+  {
+    "revision": "da76915fe2f6e94a25e9",
+    "url": "/NetflixClone/static/js/2.e3e6b94b.chunk.js"
+  },
+  {
+    "revision": "f65620f21bb2e48d5351",
+    "url": "/NetflixClone/static/js/main.fd7c5de2.chunk.js"
+  },
+  {
+    "revision": "ac96cd940339047c5bc0",
+    "url": "/NetflixClone/static/js/runtime~main.45512f5e.js"
+  },
+  {
+    "revision": "149a098f51438682ddb64f6e0c0c709b",
+    "url": "/NetflixClone/static/media/landingPage.149a098f.jpg"
+  },
+  {
+    "revision": "8562b6565f5ae1db5e4af40d85b4ed2d",
+    "url": "/NetflixClone/static/media/netflix.8562b656.png"
+  },
+  {
+    "revision": "991556c2526eb97f8e745b79363c8661",
+    "url": "/NetflixClone/static/media/normal.991556c2.jpg"
+  },
+  {
+    "revision": "c2b89779dd6b85078fc0cc2f3a9aaf28",
+    "url": "/NetflixClone/static/media/profile.c2b89779.jpg"
+  }
+]);
